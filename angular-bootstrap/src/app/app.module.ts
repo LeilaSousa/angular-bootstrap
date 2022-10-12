69d38648493746cf90871716d284a8cdeaa9b225
LeilaSousa <leilacb0@hotmail.com>
2022-10-12 15:45:09 -0300

    comecei validações

--- angular-bootstrap/src/app/app.module.ts
+++ angular-bootstrap/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +27,6 @@ import { HomeConteudoComponent } from './home-conteudo/home-conteudo.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-bootstrap/src/app/form/form.component.ts
+++ angular-bootstrap/src/app/form/form.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  onSubmit(form:any){
+console.log('form')
+  }
 
   constructor() { }
 
